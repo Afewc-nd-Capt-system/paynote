bf0b17a613feea928dbf00071550e2e1ba7d0986
Quantikz <77287598+Quantikz@users.noreply.github.com>
2026-06-04 10:52:35 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon-192.svg', 'icon-512.svg'],
+      includeAssets: ['icon-192.png', 'icon-512.png'],
       manifest: {
         name: 'Paynote',
         short_name: 'Paynote',
@@ -21,15 +21,15 @@ export default defineConfig({
         categories: ['business', 'productivity'],
         icons: [
           {
-            src: '/icon-192.svg',
+            src: '/icon-192.png',
             sizes: '192x192',
-            type: 'image/svg+xml',
+            type: 'image/png+xml',
             purpose: 'any maskable'
           },
           {
-            src: '/icon-512.svg',
+            src: '/icon-512.png',
             sizes: '512x512',
-            type: 'image/svg+xml',
+            type: 'image/png+xml',
             purpose: 'any maskable'
           }
         ]
